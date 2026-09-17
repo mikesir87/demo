2510bbaf3def9609e51344a78131f3e2d1163f4a
Michael Irwin <mikesir87@gmail.com>
2026-09-17 09:50:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the demo Express app!");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
+});
