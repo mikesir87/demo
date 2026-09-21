402192ee7e6c4cfb4e114f5e9b2c731cc3f69ae6
Michael Irwin <mikesir87@gmail.com>
2026-09-21 13:01:03 -0400

    Add /greeting API endpoint with optional name parameter

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.send("Hello from the demo Express app!");
 });
 
+app.get("/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.json({ greeting: `Hello, ${name}!` });
+});
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://0.0.0.0:${port}`);
 });
